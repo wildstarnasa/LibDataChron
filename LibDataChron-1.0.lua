08e25950b8c40033cc65a4726782c966fee9a0d6
Sinaloit <Sinaloit@gmail.com>
2014-06-13 19:29:58 -0700

    Initial Import Needs Doc work.

---
+++ LibDataChron-1.0.lua
@@ -0,0 +1,92 @@
+local MAJOR,MINOR = "Lib:DataChron-1.0", 1
+-- Get a reference to the package information if any
+local APkg = Apollo.GetPackage(MAJOR)
+-- If there was an older version loaded we need to see if this is newer
+if APkg and (APkg.nVersion or 0) >= MINOR then
+	return -- no upgrade needed
+end
+-- Set a reference to the actual package or create an empty table
+local Lib = APkg and APkg.tPackage or {}
+
+Lib.AttributeStorage, Lib.NameStorage, Lib.ProxyStorage = Lib.AttributeStorage or {}, Lib.NameStorage or {}, Lib.ProxyStorage or {}
+local AttributeStorage, NameStorage = Lib.AttributeStorage, Lib.NameStorage
+local callbacks
+
+Lib.domt = {
+	__metatable = "access denied",
+	__index = function(self, key) return AttributeStorage[self] and AttributeStorage[self][key] end,
+}
+
+Lib.domt.__newindex = function(self, key, value)
+	if not AttributeStorage[self] then AttributeStorage[self] = {} end
+	if AttributeStorage[self][key] == value then return end
+	AttributeStorage[self][key] = value
+	local name = NameStorage[self]
+	if not name then return end
+	callbacks:Fire("LibDataChron_AttributeChanged", name, key, value, self)
+	callbacks:Fire("LibDataChron_AttributeChanged_"..name, name, key, value, self)
+	callbacks:Fire("LibDataChron_AttributeChanged_"..name.."_"..key, name, key, value, self)
+	callbacks:Fire("LibDataChron_AttributeChanged__"..key, name, key, value, self)
+end
+
+function Lib:NewDataObject(name, dataobj)
+	if self.ProxyStorage[name] then return end
+
+	if dataobj then
+		assert(type(dataobj) == "table", "Invalid dataobj, must be nil or a table")
+		self.AttributeStorage[dataobj] = {}
+		for i,v in pairs(dataobj) do
+			self.AttributeStorage[dataobj][i] = v
+			dataobj[i] = nil
+		end
+	end
+	dataobj = setmetatable(dataobj or {}, self.domt)
+	self.ProxyStorage[name], self.NameStorage[dataobj] = dataobj, name
+	self.callbacks:Fire("LibDataChron_DataObjectCreated", name, dataobj)
+	return dataobj
+end
+
+function Lib:DataObjectIterator()
+	return pairs(self.ProxyStorage)
+end
+
+function Lib:GetDataObjectByName(dataobjectname)
+	return self.ProxyStorage[dataobjectname]
+end
+
+function Lib:GetNameByDataObject(dataobject)
+	return self.NameStorage[dataobject]
+end
+
+local next = pairs(AttributeStorage)
+function Lib:pairs(dataobject_or_name)
+	local t = type(dataobject_or_name)
+	assert(t == "string" or t == "table", "Usage: ldc:pairs('dataobjectname') or ldc:pairs(dataobject)")
+
+	local dataobj = self.ProxyStorage[dataobject_or_name] or dataobject_or_name
+	assert(AttributeStorage[dataobj], "Data object not found")
+
+	return next, AttributeStorage[dataobj], nil
+end
+
+local ipairs_iter = ipairs(AttributeStorage)
+function Lib:ipairs(dataobject_or_name)
+	local t = type(dataobject_or_name)
+	assert(t == "string" or t == "table", "Usage: ldc:ipairs('dataobjectname') or ldc:ipairs(dataobject)")
+
+	local dataobj = self.ProxyStorage[dataobject_or_name] or dataobject_or_name
+	assert(AttributeStorage[dataobj], "Data object not found")
+
+	return ipairs_iter, AttributeStorage[dataobj], 0
+end
+
+function Lib:OnDependencyError(strDep, strError)
+	return false
+end
+
+function Lib:OnLoad()
+	Lib.callbacks = Lib.callbacks or Apollo.GetPackage("Gemini:CallbackHandler-1:0").tPackage:New(Lib)
+	callbacks = Lib.callbacks
+end
+
+Apollo.RegisterPackage(Lib, MAJOR, MINOR, {"Gemini:CallbackHandler-1.0"})
--- README.md
+++ README.md
@@ -2,3 +2,20 @@ LibDataChron
 ============
 
 LibDataChron is a Wildstar implementation of LibDataBroker
+This relies on [CallbackHandler](https://github.com/wildstarnasa/CallbackHandler)
+
+For the moment rough docs can be found at the original github site: [LibDataBroker](https://github.com/tekkub/libdatabroker-1-1)
+
+Callback names are:
+`
+LibDataChron_AttributeChanged
+LibDataChron_AttributeChanged_
+LibDataChron_AttributeChanged_
+LibDataChron_AttributeChanged__
+`
+
+```lua
+local ldc = Apollo.GetPackage("Lib:DataChron-1.0").tPackage
+
+-- do stuff with ldc
+```
